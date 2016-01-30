d0c4c410224ceccfecff8b2f3b9e5cb5fadb77fe
kaustav haldar <hi@kaustav.me>
2016-01-29 18:57:05 -0800

    remove dependency on luvit-line-emitter

--- test.lua
+++ test.lua
@@ -14,8 +14,7 @@ end
 local txt = [[Line1
 #commentline
 
-Line2
-]]
+Line2]]
 local tmp_file = './test_read.txt'
 
 require('tap')(function(test)
@@ -42,6 +41,26 @@ require('tap')(function(test)
   test('Test for readStream', function(expect) 
     local rs = readers.readStream
     fs.writeFileSync(tmp_file, txt)
+    local readData = {}
+    local function onEnd()
+      local expected = 'Line1#commentlineLine2'
+      local readString = table.concat(readData)
+      assert(expected == readString)
+      fs.unlinkSync(tmp_file)
+    end
+
+    local readStream = rs(tmp_file)
+    local reader = Reader:new()
+    reader:once('end', onEnd)
+    reader:on('data', function(data)
+      table.insert(readData, data)
+    end)
+    readStream:pipe(reader)
+  end)
+
+  test('Test for readStream with extra ending newline', function(expect) 
+    local rs = readers.readStream
+    fs.writeFileSync(tmp_file, txt..'\n')
     local readData = {}
     local function onEnd()
       local expected = 'Line1#commentlineLine2'
